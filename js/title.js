29f3884f4117a4d5b1a6f0819b0c255185fc428d
simswipe <65012329+simswipe@users.noreply.github.com>
2023-03-10 00:35:05 +0200

    Delete js directory

--- js/title.js
+++
@@ -1,6 +0,0 @@
-(function titleScroller(text) {
-    document.title = text;
-    setTimeout(function () {
-      titleScroller(text.substr(1) + text.substr(0, 1));
-    }, 350);
-  }(" killa beamed you"));
