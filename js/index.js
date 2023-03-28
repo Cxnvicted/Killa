2a593fa13c6c2afa3794f051c90dd5d8d6c9e647
Cxnvicted <65012329+Cxnvicted@users.noreply.github.com>
2023-03-29 01:00:45 +0300

    Delete js directory

--- js/index.js
+++
@@ -1,8 +0,0 @@
-
-let titles = ['Greetings.','We Are.','Killa.','Devils Reject','Threat To Society']
-let index = 0
-let interval = setInterval(() => {
-    document.title = titles[index]
-    index += 1
-    if (index === titles.length) index = 0
-}, 500)
